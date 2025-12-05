c7bfeb5b43eb67f64fbf4895c474f52657831bcb
hoojimoojii <hoojimoojii@gmail.com>
2025-12-05 22:33:41 +0800

    save

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const pages = document.querySelectorAll(".page");
-const translateAmount = 100; 
 const book = document.getElementsByClassName("container-hand-book")[0];
 const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
 let translate = true;
 
 slide = (direction) => {
 
-    direction === "next" ? translate -= translateAmount : translate += translateAmount;
+    direction === "next" ? translate -= 100 : translate += 100;
 
     pages.forEach(
         pages => (pages.style.transform = `translateX(${translate}%)`)
@@ -16,11 +15,12 @@ slide = (direction) => {
 
 addEventListener("mousemove", (e) => { 
     mouseY = e.pageY;
-    translateY = 'translateY(' + mouseY + 'px)';
+    translateY = 'translateY(' + (mouseY+500) + 'px)';
 
-    if (translate && mouseY + book.getBoundingClientRect().height/2 < viewportHeight/2+400) {
+    if (translate && mouseY + book.getBoundingClientRect().height/2 <= viewportHeight/2+500) {
         book.style.transform = translateY;
     } else {
+        book.style.transform = 'translateY(' + (viewportHeight/2+1000-book.getBoundingClientRect().height/2) + 'px)';
         translate = false
     }
     
